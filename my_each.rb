3cd7d33aa84c1fdccc83c564e520d63e8325cadc
github username <github email address>
2019-06-07 18:27:27 -0400

    created my_each method done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,26 @@
-def my_each # put argument(s) here
+# Accepts an array argument
+# Uses #while loop to iterate over each array member, yielding each element contained in the array to a block
+# Acts like the #each method provided by Ruby
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+
+  array
 end
+
+# # Example:
+# collection = [1, 2, 3, 4]
+# my_each(collection) do |i|
+#   PUTS i
+# end
+
+# #=>
+# 1
+# 2
+# 3
+# 4
